ee7b9abdd9eb38d802ead30fc589cc1dc9205960
Walid Chiakh <walidchiakh@MacBook-Air-de-Walid.local>
2022-10-04 11:12:27 +0200

    merge: conflict

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,14 @@
 
-<footer>
-    <p>© IPSSI BD 23.2 Thomas, Ouafae, Walid</p>
+<footer class="border-top border-3">
+    <div class="d-flex justify-content-between "> 
+        <p class="text-end ">© IPSSI BD 23.2 Thomas, Ouafae, Walid</p>
 
+        <div>
+            <a href="#">A Propos</a> 
+            <a class="text-center" href="#">Contactez-nous</a>
+        </div>
+    </div>
 </footer>
-</body>
-</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+ 
---
+++ views/listEmployes.php
@@ -0,0 +1,82 @@
+
+<?php
+include('../views/header.php');
+include('../functions/apiGet.php');
+
+$urlKey = 'getEmployes';
+
+$newArray = getApi($urlKey, $array);
+
+echo '<pre>';
+//var_dump($newArray);
+echo '</pre>';
+?>
+<div class="card-group row">
+<?php
+$i = 0;
+foreach($newArray as $employe){
+    $i++;
+    ?>
+    <div class="col-sm-6 col-md-10">
+        <div class="card mb-3" style="max-width: 900px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <?php 
+                    if($employe['sexe']=== 'woman'){
+                        echo  '<img src="../assets/img/femme_icon.webp" class="img-fluid rounded-start" alt="Man">';
+                    } else {
+                        echo  '<img src="../assets/img/homme_icon.webp" class="img-fluid rounded-start" alt="Man">';
+                    }
+                    ?>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <div class='d-flex justify-content-between'>
+                        <h5 class="card-title"><?php echo strtoupper($employe['nom']).' '.ucfirst($employe['prenom']).' ('.$employe['age'].' ans)'?></h5>
+                        <span> <?php echo $employe['poste_name'][0] ?></span>
+
+                </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <p><span class="fw-bold">Adresse:</span> <?php echo ucwords($employe['adresse'])?></p>
+                            </div>
+                        </div>
+
+                        <div class="mt-4"> 
+
+                        <div class="row">
+                            <div class="col-md-4">
+                            <span class="fw-bold">Ville:</span> <?php echo ucwords($employe['ville'])?>
+                            </div>
+                            <div class="col-md-4 offset-md-2"><span class="fw-bold">Code Postal:</span> <?php echo ucwords($employe['code_postal'])?></div>
+                        </div>
+
+                        <div class="mt-5"> 
+         </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                            <span class="fw-bold">Tél:</span> <?php echo ucwords($employe['num_tel'])?>
+                            </div>
+                            <div class="col-md-6 offset-md-2"><span class="fw-bold">Mail:</span> <?php echo strtolower($employe['email'])?></div>
+                        </div>
+                    
+                </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php if($i === 2){
+        echo '</div>';
+        echo '<div class="card-group row">';
+        $i = 0;
+    }?>
+    
+    
+<?php }
+echo '</div>';
+
+include('../views/footer.php')
+?>
