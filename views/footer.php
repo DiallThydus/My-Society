062310ead0b98b28a437bc34e75b857ba8bddf7c
ouafaema <58174694+ouafaema@users.noreply.github.com>
2022-10-04 13:01:34 +0200

    chore:add navbar (#4)

--- views/footer.php
+++ views/footer.php
@@ -1,14 +1,12 @@
 <footer class="border-top border-3">
-    <div class="d-flex justify-content-between "> 
+    <div class="d-flex justify-content-between ">
         <p class="text-end ">© IPSSI BD 23.2 Thomas, Ouafae, Walid</p>
-
         <div>
-            <a href="#">A Propos</a> 
+            <a href="#">A Propos</a>
             <a class="text-center" href="#">Contactez-nous</a>
         </div>
     </div>
-</footer> 
-
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Society</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
+    <link href="../assets/ms.css" rel="stylesheet">
 </head>
 
-<body class="container">
-
-    
-
-
-
-            
+<body>
+    <?php
+    include('../views/navBar.php');
+    ?>
