773d2b6c1bcba474cebdd8fe7c03b10e27e0f649
Diall Thydus <pro.thomas.perez@gmail.com>
2022-10-04 09:08:16 +0200

    chore: functions + employees list front

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>© IPSSI BD 23.2 Thomas, Ouafae, Walid</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
